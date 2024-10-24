626007d762389fc4364e6035ff21adef09b76f4b
Isabella <aarizonastrange@gmail.com>
2024-10-24 14:15:58 +0000

    fiz a migração

---
+++ blog-app/app/Http/Controllers/PostagrmController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostagrmRequest;
+use App\Http\Requests\UpdatePostagrmRequest;
+use App\Models\Postagrm;
+
+class PostagrmController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostagrmRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Postagrm $postagrm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Postagrm $postagrm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostagrmRequest $request, Postagrm $postagrm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Postagrm $postagrm)
+    {
+        //
+    }
+}
